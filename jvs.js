474dda2953cc7f6113e65ac1fc0d8e5754d0817a
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-20 22:27:55 +0300

    Added function to populate the display Added classes to the buttons in html Added event listener to numbers button

--- jvs.js
+++ jvs.js
@@ -31,6 +31,24 @@ function operate(a, operator, b) {
     }
 }
 
+function display(a) {
+    if (result.value == 0)
+        result.value = ""
+    result.value = result.value + a;
+    displayValue = result.value;
+}
+
+const result = document.querySelector("#result")
+const numberButtons = document.querySelectorAll(".numbers")
+let displayValue = 0;
 let operator, a, b;
+
+numberButtons.forEach(number => {
+    number.addEventListener("click", e => {
+        display(e.target.innerText)
+    });
+});
+
+
 
 

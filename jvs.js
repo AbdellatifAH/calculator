d93ca5418db34f164ec1aade43a05a03893f70f4
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-22 01:18:46 +0300

    Solved all the "Gotchas" mentioned in the project description: -string together several operations and get the right answer -round decimal number so it won't overflow -pressing = before entering a number or an operator wont cause a problem -pressing clear, reset everything -Display a snarky error message if the user tries to divide by 0, and the calculator still works with no error -users can use decimal point

--- jvs.js
+++ jvs.js
@@ -1,3 +1,17 @@
+const display = document.querySelector("#result");
+const numberButtons = document.querySelectorAll(".numbers");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector("#clear");
+const equalButton = document.querySelector("#equal");
+const pointButton = document.querySelector("#point");
+let operator = '',
+    operator2 = '',
+    firstOperand = 0,
+    secondOperand = 0,
+    result = 0,
+    operandContainer = '',
+    clrD = false;
+
 function add(a, b) {
     return a + b;
 };
@@ -16,78 +30,123 @@ function divide(a, b) {
 
 function operate(a, operator, b) {
     let result;
-    switch (operator) {
-        case "+":
-            result = add(a,b);
-            break;
-        case "-":
-            result = subtract(a,b);
-            break;
-        case "*":
-            result = multiply(a,b);
-            break;
-        case "/":
-            result = divide(a,b);
-            break;
-        default:
-            result = "ERROR"
+
+    if (operator === "+") {
+        result = add(a, b);
+    } else if (operator === "-") {
+        result = subtract(a, b);
+    } else if (operator === "*") {
+        result = multiply(a, b);
+    } else if (operator === "/") {
+        if (b == 0){
+        dScreen("STOP IT!! OR...");
+        delay(1000).then(() => clear());
+        }
+        else
+            result = divide(a, b);
+    } else {
+        result = "ERROR";
     }
+
     return result;
 }
 
-function displayV(input) {
-    if(clrD || display.value == 0)
-    clearDisplay();
-    display.value = display.value + input;
+
+function dScreen(x) {
+    if (display.value == 0 || clrD)
+        display.value = ''
+    display.value = x;
+    clrD = false;
+
 }
 
-function clear(){
-userInput=0;
-a=0;
-b=0;
-operator="";
-display.value=0;
+function clear() {
+    operator = '';
+    operator2 = '';
+    firstOperand = 0;
+    secondOperand = 0;
+    result = 0;
+    operandContainer = '';
+    clrD = false;
+    dScreen('0')
+    numberButtons.forEach(number => {
+        number.addEventListener("click", numberButtonsF);
+    });
+    operatorButtons.forEach(operatorSign => {
+        operatorSign.addEventListener("click", operatorButtonsF);
+    });
+    equalButton.addEventListener("click", equalButtonF)
+    pointButton.addEventListener("click", pointButtonF, { once: true })
 }
 
-function clearDisplay(){
-    userInput=0;
-    display.value="";
-    clrD=false;
+function numberButtonsF(e) {
+    operandContainer = operandContainer + e.target.innerText;
+    dScreen(operandContainer)
+}
+
+function operatorButtonsF(e) {
+    if (operator) {
+        secondOperand = Number(operandContainer);
+        operandContainer = '';
+        operator2 = e.target.innerText;
+        clrD = true
+        firstOperand = operate(firstOperand, operator, secondOperand);
+        operator = operator2;
+        dScreen(firstOperand)
     }
+    else {
+        firstOperand = Number(operandContainer);
+        operandContainer = '';
+        operator = e.target.innerText;
+        clrD = true;
+    }
+    pointButton.addEventListener("click", pointButtonF, { once: true })
+}
 
-const display = document.querySelector("#result");
-const numberButtons = document.querySelectorAll(".numbers");
-const operatorButtons = document.querySelectorAll(".operator");
-const clearButton = document.querySelector("#clear");
-const equalButton = document.querySelector("#equal");
-let  operator, a, b, result=0, userInput = 0;
-let clrD=false;
+function equalButtonF() {
+    if (firstOperand != 0 && operator != 0) {
+        equalButton.removeEventListener("click", equalButtonF)
+        pointButton.removeEventListener("click", pointButtonF)
+        numberButtons.forEach(number => {
+            number.removeEventListener("click", numberButtonsF);
+        });
+        operatorButtons.forEach(operatorSign => {
+            operatorSign.removeEventListener("click", operatorButtonsF);
+        });
+        secondOperand = Number(operandContainer);
+        operandContainer = '';
+        result = operate(firstOperand, operator, secondOperand);
+        if(result)
+        dScreen(Number(result.toFixed(7)))
+
+    }
+    else {
+        dScreen("Try again");
+        delay(1000).then(() => clear());
+    }
+}
+
+function pointButtonF(e) {
+    operandContainer = operandContainer + e.target.innerText;
+    dScreen(operandContainer);
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
 
 numberButtons.forEach(number => {
-    number.addEventListener("click", e => {
-        displayV(e.target.innerText)
-        userInput=userInput+e.target.innerText;
-    });
+    number.addEventListener("click", numberButtonsF);
 });
 
-operatorButtons.forEach(operatorr => {
-    operatorr.addEventListener("click", e => {
-        a=parseInt(userInput);
-        operator=e.target.innerText;
-        result=operate(a,operator,b);
-        clrD=true;
-    });
+operatorButtons.forEach(operatorSign => {
+    operatorSign.addEventListener("click", operatorButtonsF);
 });
 
-equalButton.addEventListener("click", () =>{
-    b=parseInt(userInput);
-    clearDisplay();
-    result=operate(a,operator,b);
-    displayV(result);
-    a=result;
-})
+equalButton.addEventListener("click", equalButtonF);
 
-clearButton.addEventListener("click", () =>{
+clearButton.addEventListener("click", () => {
     clear();
 })
 
+pointButton.addEventListener("click", pointButtonF, { once: true })
